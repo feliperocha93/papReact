923c8195cc50b3be67819631fb4deb5b9a8099da
Felipe Rocha de Lucena - FLC <felipe.lucena@iteris.com.br>
2020-06-10 18:51:38 -0300

    Finalizando primeiro componente

---
+++ src/Components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  );
+}
+
+export default Header;
