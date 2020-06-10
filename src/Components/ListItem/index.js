2cdffe88cb6fd77f88e6cb04ce4c5c28160b9cf1
Felipe Rocha de Lucena - FLC <felipe.lucena@iteris.com.br>
2020-06-10 18:59:24 -0300

    Finalizando o desafio do modo anterior

---
+++ src/Components/ListItem/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles.css";
+
+function ListItem() {
+  return (
+    <div className="list-item">
+      <h2>Título</h2>
+      <p>Uma pequena demonstração do to-do list da Iteris</p>
+    </div>
+  );
+}
+
+export default ListItem;
