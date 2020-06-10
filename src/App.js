61edf0ec331caa6e6edcbf639ad44880be98d34f
Felipe Rocha de Lucena - FLC <felipe.lucena@iteris.com.br>
2020-06-10 18:53:38 -0300

    Resolvendo merge

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import Header from "./Components/Header";
+import ListItem from "./Components/ListItem";
 
 function App() {
   return (
     <div className="App">
-      <Header title="To-do Iteris" />
+      <Header title="To-do Iteris"></Header>
+      <ListItem />
     </div>
   );
 }
