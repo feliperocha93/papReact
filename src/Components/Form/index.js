33659a8eff251379a63c39cc8fa01f9cc18ef68c
Felipe Rocha de Lucena - FLC <fee.rocha@hotmail.com>
2020-06-11 14:25:22 -0300

    Adicionando Controlled Components para o form

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 function Form() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <form className="list-form">
-      <input placeholder="Título" type="text" />
-      <textarea placeholder="Descrição" />
+      <input
+        placeholder="Título"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Descrição"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button type="submit">Cadastrar</button>
     </form>
   );
